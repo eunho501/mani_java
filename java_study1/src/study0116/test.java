9f02d4e0a48e01d214114c64ba473fff5b4f6be5
eunho501 <bcb119@naver.com>
2025-01-16 12:53:11 +0900

    11541

--- java_study1/src/study0116/test.java
+++ java_study1/src/study0116/test.java
@@ -4,7 +4,31 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-	}
+        //주석- 설명이나 메모, 코드 대기, 등등
+		/*
+		 * 여러줄 주석-주석은 프로그램으로 실행되지않는 부분이다.
+		 * 주석안에 코드작성해도 실행안된다. 각코드의 설명을 작용하는 용도로 사용
+		 * 저장 단축키- strl+s
+		 * 주석단축키- strl+/
+		 * 실행 단축키- strl+f11
+		 */
+		
+		System.out.println("자바 이클립스 테스트");
+		
+		// 데이터를 사용하려면 변수를 선언해야된다.
+		// 변수를 선언 할때는 반드시 데이터타입을 정해야한다.
+		// 변수의 이름은 저장할 데이터에 맞는 의미 있는 이름으로 짓기
+		// 타입 다음 이름
+		// 정수 byte,short,int,long 크기순 long이 제일큼
+		// 실수:float, double
+		// 문자:char
+		// 논리:boolean
+		int age;
+		age=100; //age 변수에 100저장(대입연산)
+		age=27; 
+		age='가'//자바에서 ''표현은 문자를 의미
+				//"" 표현은 문자열을 의미 
+				} 
+				
 
 }
