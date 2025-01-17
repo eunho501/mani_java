2088414ef37a22c289e81170a54a6e6198b1f14b
eunho501 <bcb119@naver.com>
2025-01-17 12:49:07 +0900

    20250117 변수 기데이터타입

--- java_study1/src/study0116/test.java
+++ java_study1/src/study0116/test.java
@@ -8,9 +8,9 @@ public class test {
 		/*
 		 * 여러줄 주석-주석은 프로그램으로 실행되지않는 부분이다.
 		 * 주석안에 코드작성해도 실행안된다. 각코드의 설명을 작용하는 용도로 사용
-		 * 저장 단축키- strl+s
-		 * 주석단축키- strl+/
-		 * 실행 단축키- strl+f11
+		 * 저장 단축키- ctrl+s
+		 * 주석단축키- ctrl+/
+		 * 실행 단축키- ctrl+f11
 		 */
 		
 		System.out.println("자바 이클립스 테스트");
@@ -26,9 +26,84 @@ public class test {
 		int age;
 		age=100; //age 변수에 100저장(대입연산)
 		age=27; 
-		age='가'//자바에서 ''표현은 문자를 의미
+//		age='26'//자바에서 ''표현은 문자를 의미
 				//"" 표현은 문자열을 의미 
-				} 
+       
+			System.out.println(27);
+		
+			int num2 = 20;// 변수를 선언하면서 데이터 저장 -변수 초기화라고 부른다 
+			int num3 = 43; //num3은 43이다 라고 하면안되고 num3와 43이 같다는 표현이라 
+			               // num3에 43을 저장했다 num3에 43을 대입했다. 라고 표현
+//			System.out.println(num2 + num3); //syso 입력후 ctrl + 스페이스 누르면 만들어짐
+			
+			
+			int sum = num2+num3;
+					System.out.println(sum);
+			
+					// 사격형의 너비와 높이값으로 넓이를 구하시요
+//		        int width= 20;
+//		        int height=30;
+//		        int area= width+height;
+//				System.out.println(area);
+					
+		 int blue=20;
+		 int red =15;
+		 int white=0;
+		 //파란컵에 오렌지 주스가 20ml 있다.
+		 // 빨간컵에 딸기주스가 15ml있다.
+		 //파란컵에 딸기주스를 빨간컵에 오렌지 주스를 담으려면 어떻게 해야되는가
+		 //blue 와 red값 출력 
+		 //blue=15 red=20 이 방법은 아님
+		    white=blue;
+		    blue=red;
+		    red=white;
+		     System.out.println(blue);
+		     System.out.println(red);
+		   
+//		     var a=10;
+//		     var a=20; 자바 스크립트는 가능 자바는 불가능
+		     
+	        int birth_year=1991;
+	        //김철수가 태어난 년도를 birth_year에 저장했다. 나이는?
+//	        int year=2025;
+	       int myAge=2025-birth_year; 
+	        
+	        System.out.println(myAge);
+	        
+	        //변수란 데이터값을 저장할수있는 메모리 공간
+	        //변수의 공간에는 데이토타입에 맞는 데이터만 저장할수 있게 해줘야 한다,
+	        //변수의 선언은 데이터 타입 변수명 ( 예) int num;)
+	        //변수 이름은 카멜 표기법이 일반적이다.
+	        //변수 이름은 저장할 변수 사용목적에 맞는 이름으로 한다. 길어도 된다
+	        
+	          float fnum =2.156f;
+	          System.out.println(fnum);
+	          fnum=2.123456789f;
+	          System.out.println(fnum);
+	          double dnum =1.1234567890123546879;
+	          System.out.println(dnum);
+	          // float은 숫자뒤에 f 를붙여야 하지만 double은 안붙여도된다
+	          
+ 	          char ch='a';
+ 	          System.out.println(ch);
+ 	          
+ 	          ch='H';
+ 	          System.out.println(ch-'A');
+ 	          
+ 	          
+// 	          string name="홍길동";
+// 	           문자 char 문자열 string(클래스타입)= 계산은 되지않음
+ 	          
+ 	          boolean isApple =true; // 참 거짓 값을 가지는 타입
+ 	          System.out.println(isApple);
+ 	          
+ 	          
+ 	          
+ 	          
+ 	          
+ 	          
+	
+	} 
 				
 
 }
---
+++ java_study1/src/study0117/javala.java
@@ -0,0 +1,21 @@
+package study0117;
+
+public class javala {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         int num=20;
+         float pi=3.14f;
+         
+//         num=pi; 범위때문에 되지않음 실수가 정수르 포함하기 때문 
+       // num=(int)pi; 일시적으로 크기를 줄임 강제형변환 
+       // 데이터 타입 줄이는법 강제형변환: 작은쪽에 큰쪽이 들어갈때 일시적 타입변환
+//         자동형변환:큰쪽에 작은쪽이 들어갈때 정수를 실수로 바꿈
+         
+         pi=num;
+         
+		
+		
+	}
+
+}
