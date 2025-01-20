ef1804d94b7663d6e18df8d043108d1f8d1c342e
eunho501 <bcb119@naver.com>
2025-01-20 12:53:56 +0900

    2025-01-20

--- java_study1/src/study0116/test.java
+++ java_study1/src/study0116/test.java
@@ -13,8 +13,8 @@ public class test {
 		 * 실행 단축키- ctrl+f11
 		 */
 		
-		System.out.println("자바 이클립스 테스트");
-		
+//		System.out.println("자바 이클립스 테스트");
+//		
 		// 데이터를 사용하려면 변수를 선언해야된다.
 		// 변수를 선언 할때는 반드시 데이터타입을 정해야한다.
 		// 변수의 이름은 저장할 데이터에 맞는 의미 있는 이름으로 짓기
@@ -23,22 +23,22 @@ public class test {
 		// 실수:float, double
 		// 문자:char
 		// 논리:boolean
-		int age;
-		age=100; //age 변수에 100저장(대입연산)
-		age=27; 
+//		int age;
+//		age=100; //age 변수에 100저장(대입연산)
+//		age=27; 
 //		age='26'//자바에서 ''표현은 문자를 의미
 				//"" 표현은 문자열을 의미 
        
-			System.out.println(27);
-		
-			int num2 = 20;// 변수를 선언하면서 데이터 저장 -변수 초기화라고 부른다 
-			int num3 = 43; //num3은 43이다 라고 하면안되고 num3와 43이 같다는 표현이라 
-			               // num3에 43을 저장했다 num3에 43을 대입했다. 라고 표현
-//			System.out.println(num2 + num3); //syso 입력후 ctrl + 스페이스 누르면 만들어짐
-			
-			
-			int sum = num2+num3;
-					System.out.println(sum);
+//			System.out.println(27);
+//		
+//			int num2 = 20;// 변수를 선언하면서 데이터 저장 -변수 초기화라고 부른다 
+//			int num3 = 43; //num3은 43이다 라고 하면안되고 num3와 43이 같다는 표현이라 
+//			               // num3에 43을 저장했다 num3에 43을 대입했다. 라고 표현
+////			System.out.println(num2 + num3); //syso 입력후 ctrl + 스페이스 누르면 만들어짐
+//			
+//			
+//			int sum = num2+num3;
+//					System.out.println(sum);
 			
 					// 사격형의 너비와 높이값으로 넓이를 구하시요
 //		        int width= 20;
@@ -46,60 +46,71 @@ public class test {
 //		        int area= width+height;
 //				System.out.println(area);
 					
-		 int blue=20;
-		 int red =15;
-		 int white=0;
-		 //파란컵에 오렌지 주스가 20ml 있다.
-		 // 빨간컵에 딸기주스가 15ml있다.
-		 //파란컵에 딸기주스를 빨간컵에 오렌지 주스를 담으려면 어떻게 해야되는가
-		 //blue 와 red값 출력 
-		 //blue=15 red=20 이 방법은 아님
-		    white=blue;
-		    blue=red;
-		    red=white;
-		     System.out.println(blue);
-		     System.out.println(red);
-		   
+//		 int blue=20;
+//		 int red =15;
+//		 int white=0;
+//		 //파란컵에 오렌지 주스가 20ml 있다.
+//		 // 빨간컵에 딸기주스가 15ml있다.
+//		 //파란컵에 딸기주스를 빨간컵에 오렌지 주스를 담으려면 어떻게 해야되는가
+//		 //blue 와 red값 출력 
+//		 //blue=15 red=20 이 방법은 아님
+//		    white=blue;
+//		    blue=red;
+//		    red=white;
+//		     System.out.println(blue);
+//		     System.out.println(red);
+//		   
 //		     var a=10;
-//		     var a=20; 자바 스크립트는 가능 자바는 불가능
-		     
-	        int birth_year=1991;
-	        //김철수가 태어난 년도를 birth_year에 저장했다. 나이는?
-//	        int year=2025;
-	       int myAge=2025-birth_year; 
-	        
-	        System.out.println(myAge);
-	        
-	        //변수란 데이터값을 저장할수있는 메모리 공간
-	        //변수의 공간에는 데이토타입에 맞는 데이터만 저장할수 있게 해줘야 한다,
-	        //변수의 선언은 데이터 타입 변수명 ( 예) int num;)
-	        //변수 이름은 카멜 표기법이 일반적이다.
-	        //변수 이름은 저장할 변수 사용목적에 맞는 이름으로 한다. 길어도 된다
-	        
-	          float fnum =2.156f;
-	          System.out.println(fnum);
-	          fnum=2.123456789f;
-	          System.out.println(fnum);
-	          double dnum =1.1234567890123546879;
-	          System.out.println(dnum);
-	          // float은 숫자뒤에 f 를붙여야 하지만 double은 안붙여도된다
-	          
- 	          char ch='a';
- 	          System.out.println(ch);
- 	          
- 	          ch='H';
- 	          System.out.println(ch-'A');
- 	          
- 	          
-// 	          string name="홍길동";
-// 	           문자 char 문자열 string(클래스타입)= 계산은 되지않음
- 	          
- 	          boolean isApple =true; // 참 거짓 값을 가지는 타입
- 	          System.out.println(isApple);
- 	          
+////		     var a=20; 자바 스크립트는 가능 자바는 불가능
+//		     
+//	        int birth_year=1991;
+//	        //김철수가 태어난 년도를 birth_year에 저장했다. 나이는?
+////	        int year=2025;
+//	       int myAge=2025-birth_year; 
+//	        
+//	        System.out.println(myAge);
+//	        
+//	        //변수란 데이터값을 저장할수있는 메모리 공간
+//	        //변수의 공간에는 데이토타입에 맞는 데이터만 저장할수 있게 해줘야 한다,
+//	        //변수의 선언은 데이터 타입 변수명 ( 예) int num;)
+//	        //변수 이름은 카멜 표기법이 일반적이다.
+//	        //변수 이름은 저장할 변수 사용목적에 맞는 이름으로 한다. 길어도 된다
+//	        
+//	          float fnum =2.156f;
+//	          System.out.println(fnum);
+//	          fnum=2.123456789f;
+//	          System.out.println(fnum);
+//	          double dnum =1.1234567890123546879;
+//	          System.out.println(dnum);
+//	          // float은 숫자뒤에 f 를붙여야 하지만 double은 안붙여도된다
+//	          
+// 	          char ch='a';
+// 	          System.out.println(ch);
+// 	          
+// 	          ch='H';
+// 	          System.out.println(ch-'A');
+// 	          
+// 	          
+//// 	          string name="홍길동";
+//// 	           문자 char 문자열 string(클래스타입)= 계산은 되지않음
+// 	          
+// 	          boolean isApple =true; // 참 거짓 값을 가지는 타입
+// 	          System.out.println(isApple);
+// 	          
 // 	          ㅇㄴㅇ
  	          
- 	          
+ 	    int 단축수명= 2;
+ 	    int 일년= 365;
+ 	    int 흡연기간=20;
+ 	    int 하루담배=5; //하루에 5개비씩매일 흡연
+ 	    
+ 	    int 단축수명 = 하루담배* 단개피단축 *일년*흡연기간;
+ 	    int 단축수명시간=단축수명/60;
+ 	    int 단축수명일=단축수명시간/24;
+ 	    
+ 	    System.out.println("총 단축시간(분)"+단축수명);
+        System.out.println("총 단축수명"+단축수명시간);
+        System.out.println("총단축수명일"+단축수명일);
  	          
  	          
 	
---
+++ java_study1/src/study0120/java0120.java
@@ -0,0 +1,42 @@
+package study0120;
+
+import java.util.Scanner;
+public class java0120 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//변수= 데이터를 저장히가 위한 메모리공간 
+		// 변수의 이름 = 데이터가 저장된 공간을 사용하기 위해 부여한 별명
+		// 변순 선언 방법= 데이터타입 변수이름 (예- int num;)
+		//저장할 테이터에 맞는 테이터 타입을 설정 해야한다.
+		//변수 이름은 첫글자는 문자로 자바 예약어는 사용불가 
+		
+		
+		
+		//자바에서 결과로 출력하는 방법
+		//syso ctr+스페이스
+		//자바에서 키보드를 통해 데이터를 입력하는 방법
+		//자바에서 입력하기 위한 순서
+		// 1. 입력 객체 생성
+		// 2. 입력 객체를 통해 데이터에 맞는 입력메서드 호출 
+		
+		
+		// Scanner 변수이름 = new Scanner(System.In);
+	    Scanner input=new Scanner(System.In); 
+		
+	    
+	    // num이라는 변수에 정수 데이터 입력하여 저장하기 
+	    //nextInt()가 정수 데이터 입력하기 위한 메서드이다 
+	    int num=input.nextInt();
+	
+	
+	
+	
+	
+	
+	}
+	
+	
+	
+
+}
