b1d28661b2aee6f6d977176440dc3a41318f8296
eunho501 <bcb119@naver.com>
2025-02-05 12:51:25 +0900

    20250205

---
+++ java_study1/src/ja2025_01_24/java20250205.java
@@ -0,0 +1,87 @@
+package ja2025_01_24;
+
+import java.util.Scanner;
+
+public class java20250205 {
+	//자바의 변수 종류
+	//지역변수 클래스변수(static),인스턴스변수
+
+	static Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		
+//		for(int i=1;i<=2;i++) {
+//			
+//			System.out.println("변수i 사용 반복문"+i);
+//			for(int k=1;k<=3;k++	) {
+//				System.out.println("ㅂ변수k사용"+k);
+//			}
+		
+		
+		for(int i=2;i<=9;i++) {
+			for(int k=1;k<=9;k++) {
+				System.out.printf("%d*%d=%d \n",i,k,k*i);
+		}
+		
+//		int i=1; //이런경우라면 반복문 두개를 쓰는게 더좋을수있다. 
+//		for(int k=2;k<=9;) {
+//			System.out.println(k+"*"+i+"="+(k*i));
+//			i++;
+//			if(i==10) {
+//				k++;
+//				i=1;
+//			}
+//		}
+//		
+//		
+		
+		
+//		int num=0;
+//		
+//		for(;num!=100;) {
+//			System.out.println("정수입력:");
+//			num=sc.nextInt();
+//			
+		}
+		//for(;;)	for(;true;) for(;num!=0;) 무한루프 
+//		while(true) {//while문의 무한루프 
+//			
+//		}
+//		for(;;){//for문 무한루프
+//		System.out.println("정수입력");
+//		int num=sc.nextInt();
+//		if(num==0)break;//무한루프의 종류시점 정하기 if 문과 break 로 
+//		}
+//		//break는 해당 제어문(반복문 switch)을 종료시키기만 한다
+//		//return은 메서드를 종료 
+//		System.out.println("무한루프가 종료");
+////		for(int i=1;;i++) {//무한루프 
+////			System.out.println(i);
+////		}
+//		
+		
+		//10부터 100까지 총합을 구하시오
+		//단 10단위숫자만 계산
+//		int total=0;
+//		for(int i=10; i<=100; i=i+10) {
+//			total+=i; //
+//		}
+//		System.out.println(total);
+//			
+		
+		
+		
+		
+		
+		
+		
+		
+//		for(초기값; 조건식; 증감식) {
+//			반복내용
+//		}
+//		초기값->조건식비교 ->참이라면->{내용}->증감식
+//		->조건식비교->참이라면->{내용}->증감식
+//		->조건식비교->참이라면->{내용}->증감식
+//		->조건식비교->거짓이라면-> 종료
+	}
+
+}
