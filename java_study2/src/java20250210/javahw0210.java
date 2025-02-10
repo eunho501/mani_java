346f0d834e86c43f83fc4f9774ea288737eb0a93
JangEunSeok <bcb119@naver.com>
2025-02-10 22:19:57 +0900

    1231231

---
+++ java_study2/src/java20250210/javahw0210.java
@@ -0,0 +1,19 @@
+package java20250210;
+
+public class javahw0210 {
+
+	public static void main(String[] args) {
+		int[]num=new int[15];
+		int count=0;
+		
+		
+		while(count<15) {
+			count=(int)(Math.random()*50+1);
+			if(count%2==0) {
+				
+			}
+		}
+		System.out.println("짝수");
+	}
+
+}
