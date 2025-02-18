d2389bc8b97c8619157048efe291fa5cb2426f39
eunho501 <bcb119@naver.com>
2025-02-18 12:48:17 +0900

    2025 02 18

---
+++ javaclass1/src/study0218/ClassArray.java
@@ -0,0 +1,48 @@
+package study0218;
+
+public class ClassArray {
+
+	public static void main(String[] args) {
+		
+		
+//		Member mem=new Member();
+//		mem.name="장덕철";
+//		mem.job="가수";
+//		mem.age=31;
+//String name=new String();		
+// String[]	names=new String[3];	
+//		Member[] members=new Member[5];
+//		//5명의 정보저장 가능한데 아직아직아직
+//		for(int i=0;i<members.length;i++)
+//			members[i]=new Member();
+//		
+//		members[0].job="감찰";
+//		members[0].name="이몁박";
+//		members[0].age=34;
+//		members[1].job="군인";
+//		members[1].name="이순신";
+//		members[1].age=23;
+//		members[2].name="문익점";
+//		members[2].age=24;
+//		members[2].job="산언스파이";
+//		
+//		//빨간줄이 안뜨지만 오류이다 
+//		for(int i=0;i<members.length;i++) {
+//			System.out.println(members[i].age);
+//			System.out.println(members[i].name);
+//			System.out.println(members[i].job);
+//		}
+//		System.out.println(members[0].name);
+//		
+//		int[] a=new int[] {1,2,3,4,5,6};
+		
+		Member info=new Member();
+		
+		
+		
+		
+		
+		
+	}
+
+}
